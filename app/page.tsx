f8dca61197cb579d261cf4b436461d6cf395221e
PeakMNA <122144475+PeakMNA@users.noreply.github.com>
2024-09-23 17:15:44 +0700

    test error commit

--- app/page.tsx
+++ app/page.tsx
@@ -12,6 +12,8 @@ export default function HomePage() {
         <Link href="/dashboard" className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
           Go to Menu
         </Link>
+    abc
+        <TEST />
       </div>
     </div>
   );
